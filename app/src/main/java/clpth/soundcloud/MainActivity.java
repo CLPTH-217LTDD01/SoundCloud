dc1e66f51dbd9aa414ed9a0e3ef81838b69c370b
QuoCuong <quocuong@users.noreply.github.com>
2018-03-29 21:32:49 +0700

    Added BottomNavigationView

--- app/src/main/java/clpth/soundcloud/MainActivity.java
+++ app/src/main/java/clpth/soundcloud/MainActivity.java
@@ -1,7 +1,17 @@
 package clpth.soundcloud;
 
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bnv = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        disableShiftMode(bnv);
+        bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        MainActivity.this.getSupportActionBar().setTitle("Home");
+                        selectedFragment = HomeFragment.newInstance();
+                        break;
+                    case R.id.action_stream:
+                        MainActivity.this.getSupportActionBar().setTitle("Stream");
+                        selectedFragment = StreamFragment.newInstance();
+                        break;
+                    case R.id.action_search:
+                        MainActivity.this.getSupportActionBar().setTitle("");
+                        selectedFragment = SearchFragment.newInstance();
+                        break;
+                    case R.id.action_favorite:
+                        MainActivity.this.getSupportActionBar().setTitle("Collection");
+                        selectedFragment = FavoriteFragment.newInstance();
+                        break;
+                    case R.id.action_options:
+                        MainActivity.this.getSupportActionBar().setTitle("More");
+                        selectedFragment = OptionsFragment.newInstance();
+                        break;
+                }
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_layout, selectedFragment);
+                transaction.commit();
+                return true;
+            }
+        });
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, HomeFragment.newInstance());
+        transaction.commit();
+    }
+
+
+
+    public void disableShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("BNVHelper", "Unable to get shift mode field");
+        } catch (IllegalAccessException e) {
+            Log.e("BNVHelper", "Unable to change value of shift mode");
+        }
     }
 }
---
+++ app/src/main/java/clpth/soundcloud/StreamFragment.java
@@ -0,0 +1,25 @@
+package clpth.soundcloud;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StreamFragment extends Fragment {
+    public static StreamFragment newInstance() {
+        StreamFragment fragment = new StreamFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_stream, container, false);
+    }
+}
